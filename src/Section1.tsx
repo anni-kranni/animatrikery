15d495a539b80d0d76fcbf4af8b227b3c7b9fc56
Anni-Kranni <eanro010@edu.xamk.fi>
2025-01-21 16:44:16 +0200

    section 1

---
+++ src/Section1.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useRef } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react';
+import './App.css';
+
+function Section1() {
+    return (
+        <div className='section'></div>
+    )
+};
+
+export default Section1;
